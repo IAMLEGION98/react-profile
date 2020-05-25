38e9334f960bc20e13ec8f6f58e64d47d63951f6
IAMLEGION98 <anupamrajanish@gmail.com>
2020-05-25 18:27:46 +0530

    Pushing initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch,Route } from "react-router-dom";
+import MainProfile from "./MainProfile";
+
 import './App.css';
 
 function App() {
+  const routes = [
+    {
+      path: "/",
+      exact: true,
+      component: () => <MainProfile />,
+    },
+    
+    
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+     <Switch>
+     {routes.map((route, index) => (
+              <Route
+                key = {index}
+                path = {route.path}
+                exact = {route.exact}
+                children = {<route.component />}
+              />
+            ))}
+     </Switch>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/MainProfile.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Button,
+  Form,
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+const MainProfile = () => {
+  return (
+    <div>
+      <header>
+        
+        <div className="row banner">
+          <div className="banner-text">
+            <h1>I'm Anupam Rajanish</h1>
+            <h2>Mobile/Web/Backend Developer, Hackathon enthusiast</h2>
+          </div>
+        </div>
+        <ul className="social">
+        <li key="instagram"><a href="https://www.instagram.com/iamlegion98/"><InstagramIcon className="svgIcons"/></a></li>
+        <li key="facebook"><a href="https://www.facebook.com/anupam.rajanish.5"><FacebookIcon className="svgIcons"/></a></li>
+        <li key="Github"><a href="https://github.com/IAMLEGION98"><GitHubIcon className="svgIcons"/></a></li>
+            </ul>
+      </header>
+    </div>
+  );
+};
+
+export default MainProfile;
